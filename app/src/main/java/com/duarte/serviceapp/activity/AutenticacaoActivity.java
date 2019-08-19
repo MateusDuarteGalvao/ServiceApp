ef4f7589ad45adcdafccd3741badf38ba1528bb7
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-19 08:12:46 -0300

    Ajustes iniciais na tela de autenticacao

--- app/src/main/java/com/duarte/serviceapp/activity/AutenticacaoActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/AutenticacaoActivity.java
@@ -2,15 +2,33 @@ package com.duarte.serviceapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import com.duarte.serviceapp.R;
 
 public class AutenticacaoActivity extends AppCompatActivity {
+
+    private Button botaoAcessar;
+    private EditText campoEmail, campoSenha;
+    private Switch tipoAcesso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autenticacao);
         getSupportActionBar().hide();
+
+        inicializaComponentes();
     }
+
+    private void inicializaComponentes() {
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        botaoAcessar = findViewById(R.id.buttonAcesso);
+        tipoAcesso = findViewById(R.id.switchAcesso);
+    }
+
+
 }
