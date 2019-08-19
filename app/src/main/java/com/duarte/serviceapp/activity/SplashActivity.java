900e3756777fac2233aeabf37cd1f04290634e44
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-18 22:14:33 -0300

    Initial commit

---
+++ app/src/main/java/com/duarte/serviceapp/activity/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.duarte.serviceapp.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duarte.serviceapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abrirAutenticacao();
+            }
+        }, 3000);
+    }
+
+    private void abrirAutenticacao() {
+        Intent i = new Intent(SplashActivity.this, AutenticacaoActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
+}
