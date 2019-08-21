260393da6abea1901bf0f931cd9049a37786b3e3
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-21 17:41:13 -0300

    Ajustes nos botoes da toolbar do prestador

---
+++ app/src/main/java/com/duarte/serviceapp/activity/NovoServicoPrestadorActivity.java
@@ -0,0 +1,15 @@
+package com.duarte.serviceapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duarte.serviceapp.R;
+
+public class NovoServicoPrestadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_servico_prestador);
+    }
+}
--- app/src/main/java/com/duarte/serviceapp/activity/PrestadorActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/PrestadorActivity.java
@@ -1,19 +1,27 @@
 package com.duarte.serviceapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.duarte.serviceapp.R;
+import com.duarte.serviceapp.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrestadorActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         //Configurações ToolBar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -30,4 +38,42 @@ public class PrestadorActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                deslogarUsuario();
+                break;
+            case R.id.menuConfiguracoes :
+                abrirConfiguracoes();
+                break;
+            case R.id.menuNovoServico :
+                abriNovoServico();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void abrirConfiguracoes() {
+        startActivity(new Intent(PrestadorActivity.this, ConfiguracaoFirebase.class));
+    }
+
+    private void abriNovoServico() {
+        startActivity(new Intent(PrestadorActivity.this, NovoServicoPrestadorActivity.class));
+    }
+
+
+
 }
