82554d87f46513114e70604648a043792756248b
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-21 12:41:58 -0300

    Ajustes na tela inicial do prestador

--- app/src/main/java/com/duarte/serviceapp/activity/PrestadorActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/PrestadorActivity.java
@@ -2,6 +2,9 @@ package com.duarte.serviceapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 
 import com.duarte.serviceapp.R;
 
@@ -11,5 +14,20 @@ public class PrestadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestador);
+
+        //Configurações ToolBar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("ServiceApp - prestador");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_prestador, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
