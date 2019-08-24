1b69942cc1544027c09c6628ec65c996d2cd5fb8
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-24 20:33:39 -0300

    Ajustes na tela inicial do cliente

--- app/src/main/java/com/duarte/serviceapp/activity/HomeActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/HomeActivity.java
@@ -3,20 +3,36 @@ package com.duarte.serviceapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.duarte.serviceapp.R;
+import com.duarte.serviceapp.adapter.AdapterPrestador;
+import com.duarte.serviceapp.adapter.AdapterServico;
 import com.duarte.serviceapp.helper.ConfiguracaoFirebase;
+import com.duarte.serviceapp.model.Prestador;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
+    private RecyclerView recyclerPrestador;
+    private List<Prestador> prestadores = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private AdapterPrestador adapterPrestador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +40,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         inicializarComponentes();
-
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         //Configurações ToolBar
@@ -32,6 +48,39 @@ public class HomeActivity extends AppCompatActivity {
         toolbar.setTitle("ServiceApp");
         setSupportActionBar(toolbar);
 
+        //Configura recyclerView
+        recyclerPrestador.setLayoutManager(new LinearLayoutManager(this));
+        recyclerPrestador.setHasFixedSize(true);
+        adapterPrestador = new AdapterPrestador(prestadores);
+        recyclerPrestador.setAdapter( adapterPrestador );
+
+        //Recupera prestadores para o cliente
+        recuperarPrestadores();
+
+    }
+
+    private void recuperarPrestadores(){
+
+        DatabaseReference prestadorRef = firebaseRef.child("prestadores");
+        prestadorRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                prestadores.clear();
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    prestadores.add( ds.getValue(Prestador.class) );
+                }
+
+                adapterPrestador.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -43,7 +92,6 @@ public class HomeActivity extends AppCompatActivity {
         //Configurar botão de pesquisa
         MenuItem item = menu.findItem(R.id.menuPesquisa);
         searchView.setMenuItem(item);
-
 
 
         return super.onCreateOptionsMenu(menu);
@@ -66,6 +114,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private void inicializarComponentes(){
         searchView = findViewById(R.id.materialSearchView);
+        recyclerPrestador = findViewById(R.id.recyclerPrestador);
     }
 
     private void deslogarUsuario(){
---
+++ app/src/main/java/com/duarte/serviceapp/adapter/AdapterPrestador.java
@@ -0,0 +1,70 @@
+package com.duarte.serviceapp.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.duarte.serviceapp.R;
+import com.duarte.serviceapp.model.Prestador;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class AdapterPrestador extends RecyclerView.Adapter<AdapterPrestador.MyViewHolder> {
+
+    private List<Prestador> prestadores;
+
+    public AdapterPrestador(List<Prestador> prestadores) {
+        this.prestadores = prestadores;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_prestador, parent, false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
+        Prestador prestador = prestadores.get(i);
+        holder.nomePrestador.setText(prestador.getNome());
+        holder.categoria.setText(prestador.getCategoria() + " - ");
+        holder.tempo.setText(prestador.getTempo() + " Min");
+        holder.precoHora.setText("R$ " + prestador.getPrecoHora().toString());
+
+        //Carregar imagem
+        String urlImagem = prestador.getUrlImagem();
+        Picasso.get().load( urlImagem ).into( holder.imagemPrestador );
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return prestadores.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imagemPrestador;
+        TextView nomePrestador;
+        TextView categoria;
+        TextView tempo;
+        TextView precoHora;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            nomePrestador = itemView.findViewById(R.id.textNomePrestador);
+            categoria = itemView.findViewById(R.id.textCategoriaPrestador);
+            tempo = itemView.findViewById(R.id.textTempoPrestador);
+            precoHora = itemView.findViewById(R.id.textPrecoHoraPrestador);
+            imagemPrestador = itemView.findViewById(R.id.imagePrestador);
+        }
+    }
+}
