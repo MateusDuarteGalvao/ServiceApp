f9928187ef3a1bdd5d6a30aa80125a46c6d65db3
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-08-29 07:53:10 -0300

    Ajustes na tela de servicos

--- app/src/main/java/com/duarte/serviceapp/activity/ServicosActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/ServicosActivity.java
@@ -1,16 +1,22 @@
 package com.duarte.serviceapp.activity;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.duarte.serviceapp.R;
 import com.duarte.serviceapp.adapter.AdapterServico;
 import com.duarte.serviceapp.helper.ConfiguracaoFirebase;
+import com.duarte.serviceapp.helper.UsuarioFirebase;
+import com.duarte.serviceapp.model.Cliente;
 import com.duarte.serviceapp.model.Prestador;
 import com.duarte.serviceapp.model.Servico;
 import com.google.firebase.database.DataSnapshot;
@@ -22,17 +28,23 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import dmax.dialog.SpotsDialog;
+
 public class ServicosActivity extends AppCompatActivity {
 
     private RecyclerView recyclerServicosPrestador;
     private ImageView imagePrestadorServico;
     private TextView textNomePrestadorServico;
     private Prestador prestadorSelecionado;
+    private AlertDialog dialog;
+
 
     private AdapterServico adapterServico;
     private List<Servico> servicos = new ArrayList<>();
     private DatabaseReference firebaseRef;
     private String idPrestador;
+    private String idUsuarioLogado;
+    private Cliente cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,7 @@ public class ServicosActivity extends AppCompatActivity {
         //Configurações iniciais
         inicializarComponentes();
         firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Recupera prestador selecionado
         Bundle bundle = getIntent().getExtras();
@@ -69,7 +82,43 @@ public class ServicosActivity extends AppCompatActivity {
 
         //Recupera serviços para o prestador
         recuperarServicos();
+        recuperarDadosCliente();
 
+    }
+
+    private void recuperarDadosCliente() {
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando dados")
+                .setCancelable(false)
+                .build();
+        dialog.show();
+
+        DatabaseReference clientesRef = firebaseRef
+                .child( "clientes" )
+                .child( idUsuarioLogado );
+
+        clientesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if( dataSnapshot.getValue() != null ){
+                    cliente = dataSnapshot.getValue(Cliente.class);
+                }
+
+                recuperarOrdemServico();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void recuperarOrdemServico() {
+
+        dialog.dismiss();
     }
 
     private void recuperarServicos(){
@@ -97,6 +146,27 @@ public class ServicosActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_servicos, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuOrdemServico :
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void inicializarComponentes(){
         recyclerServicosPrestador = findViewById(R.id.recyclerServicosPrestador);
