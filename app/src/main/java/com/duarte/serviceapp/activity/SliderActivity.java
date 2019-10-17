fab5574aae711d2015e29750b2e13212f4d40970
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-10-17 07:22:48 -0300

    Ajustes no aplicativo

--- app/src/main/java/com/duarte/serviceapp/activity/SliderActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/SliderActivity.java
@@ -2,20 +2,16 @@ package com.duarte.serviceapp.activity;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-
 import android.os.Bundle;
 
 import com.duarte.serviceapp.R;
 import com.github.paolorotolo.appintro.AppIntro;
-import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class SliderActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_slider);
 
         addSlide(AppIntroSampleSlider.newInstance(R.layout.slide01));
         addSlide(AppIntroSampleSlider.newInstance(R.layout.slide02));
