2e0be219deece649277bcd413f51840d12ab67ad
Matheus Duarte <matheusduartegalvao@gmail.com>
2019-10-17 07:44:03 -0300

    Ajustes na tela de selecao do usuario

--- app/src/main/java/com/duarte/serviceapp/activity/SelecaoUsuarioActivity.java
+++ app/src/main/java/com/duarte/serviceapp/activity/SelecaoUsuarioActivity.java
@@ -7,40 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 import com.duarte.serviceapp.R;
-import com.google.firebase.auth.FirebaseUser;
 
 public class SelecaoUsuarioActivity extends AppCompatActivity {
 
-    private Button botaoAutenticaoPrestador, botaoAutenticacaoCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecao_usuario);
 
-        //Configurações inicais
-        inicializaComponentes();
 
-        botaoAutenticacaoCliente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),
-                        AutenticacaoClienteActivity.class));
-            }
-        });
-
-        botaoAutenticaoPrestador.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),
-                        AutenticacaoPrestadorActivity.class));
-            }
-        });
     }
 
+    public void autenticarCliente(View view) {
+        Intent i = new Intent(getApplicationContext(), AutenticacaoClienteActivity.class);
+        startActivity(i);
+    }
 
-    private void inicializaComponentes() {
-        botaoAutenticacaoCliente = findViewById(R.id.botaoAutenticaoCliente);
-        botaoAutenticaoPrestador = findViewById(R.id.botaoAutenticaoPrestador);
+    public void autenticarPrestador(View view) {
+        Intent i = new Intent(getApplicationContext(), AutenticacaoPrestadorActivity.class);
+        startActivity(i);
     }
 }
